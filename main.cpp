fde2b47e33aa3ff18ae9667e8dc30925f423fb21
RafaelG <rafael.grandsire@gmail.com>
2019-07-01 21:32:14 -0300

    r

--- main.cpp
+++ main.cpp
@@ -9,10 +9,16 @@ const double SQ2 = sqrt(2.0);
 
 using namespace std;
 
-ofstream xout;
-ofstream yout;
+int it = 0;
 
-const int N = 4;
+void log(ofstream &logger, vec<double> v, double (f)(vec<double>)){
+	logger << fixed << setprecision(5);
+	logger << f(v) << '\t';
+	logger << endl;
+	it++;
+}
+
+const int N = 2;
 
 double f0(vec<double> x){
 	double ans = 0.0;
@@ -92,7 +98,7 @@ matrix<double> hessian(vec<double> x, double (f)(vec<double>)){
 	int n = x.n;
 	auto D = canonical_dir(n);
 	h = matrix<double>(n, n);
-	
+
 	double EPS = HESSIAN_EPS;
 	for (int i = 0; i < n; i++){
 		for (int j = i; j < n; j++){
@@ -170,7 +176,7 @@ pair<vec<double>, double> goldensection(vec<double> l, vec<double> r, double (f)
 	return {ans, f_ans};
 }
 
-auto line_search_method = goldensection;
+auto line_search_method = armijo;
 
 template <typename X> X right_limit(X l, X dir, double (f)(X) ){
 	auto tmpL = l;
@@ -189,45 +195,84 @@ template <typename X> X right_limit(X l, X dir, double (f)(X) ){
 }
 
 vec<double> gradient_method(vec<double> l, double (f)(vec<double>)){
+	ofstream logger("logs/gradient_method.txt");
+	it = 0;
+	auto start = chrono::steady_clock::now();
+	auto leave = [&]{
+		auto end = chrono::steady_clock::now();
+		logger
+			<< "Elapsed time in milliseconds : " 
+			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
+			<< " ms" << endl;
+		logger << it << endl;
+		return l;
+	};
 	int n = l.n;
 	vec<double> grad = gradient(l, f);
 	while (true){
-		if ((grad.trans()*grad).trace() < STOPPING_EPS) return l;
+		log(logger, l, f);
+		if ((grad.trans()*grad).trace() < STOPPING_EPS) return leave();
 		vec<double> p = grad*-1;
 		vec<double>	r = right_limit(l, p, f);
 		auto line_search = line_search_method(l, r, f);
 		vec<double> min_point = line_search.first;
-		if ((min_point-l).norm() < STOPPING_EPS) return l;
-		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return l;
+		if ((min_point-l).norm() < STOPPING_EPS) return leave();
+		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return leave();
 		l = min_point;
 		grad = gradient(l, f);
 	}
 }
 
 vec<double> newton_method(vec<double> l, double (f)(vec<double>)){
+	ofstream logger("logs/newton_method.txt");
+	it = 0;
+	auto start = chrono::steady_clock::now();
+	auto leave = [&]{
+		auto end = chrono::steady_clock::now();
+		logger
+			<< "Elapsed time in milliseconds : " 
+			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
+			<< " ms" << endl;
+		logger << it << endl;
+		return l;
+	};
 	int n = l.n;
 	vec<double> grad = gradient(l, f);
 	while (true){
+		log(logger, l, f);
 		vec<double> p = hessian(l, f).inverse()*grad*-1;
 		vec<double> min_point = l + p;
-		if ((min_point-l).norm() < STOPPING_EPS) return l;
-		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return l;
+		if ((min_point-l).norm() < STOPPING_EPS) return leave();
+		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return leave();
 		l = min_point;
 		grad = gradient(l, f);
 	}
 }
 
 vec<double> newton_method_line_search(vec<double> l, double (f)(vec<double>)){
+	ofstream logger("logs/newton_method_line_search.txt"); 
+	it = 0;
+	auto start = chrono::steady_clock::now();
+	auto leave = [&]{
+		auto end = chrono::steady_clock::now();
+		logger
+			<< "Elapsed time in milliseconds : " 
+			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
+			<< " ms" << endl;
+		logger << it << endl;
+		return l;
+	};
 	int n = l.n;
 	vec<double> grad = gradient(l, f);
 	while (true){
+		log(logger, l, f);
 		vec<double> p = hessian(l, f).inverse()*grad*-1;
 
 		vec<double>	r = right_limit(l, p, f);
 		auto line_search = line_search_method(l, r, f);
 		vec<double> min_point = line_search.first;
-		if ((min_point-l).norm() < STOPPING_EPS) return l;
-		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return l;
+		if ((min_point-l).norm() < STOPPING_EPS) return leave();
+		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return leave();
 		l = min_point;
 		grad = gradient(l, f);
 	}
@@ -237,29 +282,34 @@ vec<double> newton_method_line_search(vec<double> l, double (f)(vec<double>)){
 https://en.wikipedia.org/wiki/Davidon%E2%80%93Fletcher%E2%80%93Powell_formula
  */
 vec<double> DFP(vec<double> l, double (f)(vec<double>)){
+	cout << "DFP" << endl;
+	ofstream logger("logs/DFP.txt");
+	it = 0;
+	auto start = chrono::steady_clock::now();
+	auto leave = [&]{
+		auto end = chrono::steady_clock::now();
+		logger
+			<< "Elapsed time in milliseconds : " 
+			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
+			<< " ms" << endl;
+		logger << it << endl;
+		return l;
+	};
 	int n = l.n;
 	vec<double> grad = gradient(l, f);
 	matrix<double> I(n, n, -1);
 	matrix<double> B_ = I;
-	while (true){
-		if ((grad.trans()*grad).trace() < STOPPING_EPS) {
-			cout << 1 << endl;
-			return l;
-		}
+	int IT_MAX = 1e8;
+	while (IT_MAX--){
+		log(logger, l, f);
+		if ((grad.trans()*grad).trace() < STOPPING_EPS) return leave();
 		vec<double> p = (B_*grad)*-1;
 		vec<double>	r = right_limit(l, p, f);
 		auto line_search = line_search_method(l, r, f);
 		vec<double> min_point = line_search.first;
-		if ((min_point-l).norm() < STOPPING_EPS) {
-			cout << 2 << endl;
-			return l;
-		}
-		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) {
-			cout << 3 << endl;
-			return l;
-		}
-
-
+		if ((min_point-l).norm() < STOPPING_EPS) return leave();
+		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return leave();
+		//cout << fabs(f(min_point) - f(l)) << endl;
 		vec<double> s = min_point-l;
 
 		l = min_point;
@@ -275,24 +325,38 @@ vec<double> DFP(vec<double> l, double (f)(vec<double>)){
 		B_ += V;
 		B_ += W;
 	}
+	return leave();
 }
 
 /*
 https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm
  */
 vec<double> BFGS(vec<double> l, double (f)(vec<double>)){
+	ofstream logger("logs/BFGS.txt");
+	it = 0;
+	auto start = chrono::steady_clock::now();
+	auto leave = [&]{
+		auto end = chrono::steady_clock::now();
+		logger
+			<< "Elapsed time in milliseconds : " 
+			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
+			<< " ms" << endl;
+		logger << it << endl;
+		return l;
+	};
 	int n = l.n;
 	vec<double> grad = gradient(l, f);
 	matrix<double> I(n, n, -1);
 	matrix<double> B_ = I;
 	while (true){
-		if ((grad.trans()*grad).trace() < STOPPING_EPS) return l;
+		log(logger, l, f);
+		if ((grad.trans()*grad).trace() < STOPPING_EPS) return leave();
 		vec<double> p = (B_*grad)*-1;
 		vec<double>	r = right_limit(l, p, f);
 		auto line_search = line_search_method(l, r, f);
 		vec<double> min_point = line_search.first;
-		if ((min_point-l).norm() < STOPPING_EPS) return l;
-		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return l;
+		if ((min_point-l).norm() < STOPPING_EPS) return leave();
+		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return leave();
 
 
 		vec<double> s = min_point-l;
@@ -315,15 +379,28 @@ vec<double> BFGS(vec<double> l, double (f)(vec<double>)){
 }
 
 vec<double> conjugate_gradient_method_sq(vec<double> l, double(f)(vec<double>)){
+	ofstream logger("logs/conjugate_gradient_method_sq.txt");
+	it = 0;
+	auto start = chrono::steady_clock::now();
+	auto leave = [&]{
+		auto end = chrono::steady_clock::now();
+		logger
+			<< "Elapsed time in milliseconds : " 
+			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
+			<< " ms" << endl;
+		logger << it << endl;
+		return l;
+	};
 	auto grad = gradient(l, f);
 	auto d = grad*-1;
 	while (true){
-		if ((grad.trans()*grad).trace() < STOPPING_EPS) return l;
+		log(logger, l, f);
+		if ((grad.trans()*grad).trace() < STOPPING_EPS) return leave();
 		auto B = hessian(l, f);
 		double alpha = -(grad.trans()*d).trace()/(d.trans()*(B*d)).trace();
 		vec<double> min_point = l + d*alpha;
-		if ((min_point-l).norm() < STOPPING_EPS) return l;
-		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return l;
+		if ((min_point-l).norm() < STOPPING_EPS) return leave();
+		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return leave();
 		auto r = min_point;
 		auto next_grad = gradient(r, f);
 		double beta = (next_grad.trans()*(B*d)).trace()/(d.trans()*(B*d)).trace();
@@ -335,15 +412,28 @@ vec<double> conjugate_gradient_method_sq(vec<double> l, double(f)(vec<double>)){
 }
 
 vec<double> conjugate_gradient_method_uni(vec<double> l, double(f)(vec<double>)){
+	ofstream logger("logs/conjugate_gradient_method_uni.txt");  
+	it = 0;
+	auto start = chrono::steady_clock::now();
+	auto leave = [&]{
+		auto end = chrono::steady_clock::now();
+		logger
+			<< "Elapsed time in milliseconds : " 
+			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
+			<< " ms" << endl;
+		logger << it << endl;
+		return l;
+	};
 	auto grad = gradient(l, f);
 	auto d = grad*-1;
 	while (true){
-		if ((grad.trans()*grad).trace() < STOPPING_EPS) return l;
+		log(logger, l, f);
+		if ((grad.trans()*grad).trace() < STOPPING_EPS) return leave();
 		vec<double>	r = right_limit(l, d, f);
 		auto line_search = line_search_method(l, r, f);
 		vec<double> min_point = line_search.first;
-		if ((min_point-l).norm() < STOPPING_EPS) return l;
-		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return l;
+		if ((min_point-l).norm() < STOPPING_EPS) return leave();
+		if (fabs(f(min_point) - f(l)) < STOPPING_EPS) return leave();
 		l = min_point;
 		auto next_grad = gradient(l, f);
 		double beta = (next_grad.trans()*d).trace()/(d.trans()*d).trace();
@@ -357,6 +447,18 @@ vec<double> conjugate_gradient_method_uni(vec<double> l, double(f)(vec<double>))
 https://en.wikipedia.org/wiki/Ellipsoid_method
  */
 vec<double> ellipsoid(vec<double> x, double (f)(vec<double>)){
+	ofstream logger("logs/ellipsoid.txt");
+	it = 0;
+	auto start = chrono::steady_clock::now();
+	auto leave = [&]{
+		auto end = chrono::steady_clock::now();
+		logger
+			<< "Elapsed time in milliseconds : " 
+			<< chrono::duration_cast<chrono::milliseconds>(end - start).count()
+			<< " ms" << endl;
+		logger << it << endl;
+		return x;
+	};
 	int n = x.n;
 	matrix<double> aux(n, n), P;
 	for (int i = 0; i < n; i++)
@@ -364,12 +466,11 @@ vec<double> ellipsoid(vec<double> x, double (f)(vec<double>)){
 	double sc = (1.0*n*n)/(n*n-1);
 	P = aux;
 	while (true){
-		if (P.trace() < STOPPING_EPS)
-			return x;
+		log(logger, x, f);
+		if (P.trace() < STOPPING_EPS) return leave();
 		vec<double> g = gradient(x, f);
 		double den = sqrt((g.trans()*P*g).trace());
-		if (den < STOPPING_EPS)
-			return x;
+		if (den < STOPPING_EPS) return leave();
 
 		vec<double> g_ = g*(1.0/den);	
 
@@ -389,16 +490,21 @@ int main(){
 	uniform_real_distribution<double> distribution(-lim, lim);
 
 
-	auto f = f1; //funcao otimizada
-	auto method = conjugate_gradient_method_uni; //método de otimizacao
+	auto f = f2; //funcao otimizada
+
 	vec<double> i(N);
 	for (int j = 0; j < N; j++){
 		i(j) = distribution(rng);
-		//i(j) = 1;
 	}
-	auto ans = method(i, f);
+	i = {
+		{2.54358100},
+		{4.73342043}
+	};
 	cout << "starting point " << i << endl;
-	cout << "minimum found at " << ans << endl;
-	cout << "f(x) = " << f(ans) << endl;
+	for (auto method : {gradient_method, conjugate_gradient_method_sq, conjugate_gradient_method_uni, newton_method, newton_method_line_search, DFP, BFGS, ellipsoid}){
+		auto ans = method(i, f);
+		cout << "minimum found at " << ans << endl;
+		cout << "f(x) = " << f(ans) << endl;
+	}
 	return 0;
 }
